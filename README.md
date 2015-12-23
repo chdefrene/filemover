373db6236c657208a07694df9c96caee3ebc2d80
Christian <defrenechr@me.com>
2015-12-24 00:59:24 +0100

    Started work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # filemover
+
+Add "filemover.py" to your nautilus scripts folder.
+
+This script will move your video file to the correct tv show and season location.
--- filemover.py
+++ filemover.py
@@ -1,10 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-if sys.version_info >= (3,0):
-	import urllib.request
-	from xmlrpc.client import ServerProxy, Error
-else: # python2
-	import urllib2
-	from xmlrpclib import ServerProxy, Error
+import ntpath
+
+
+""" Set up default script variables """
+show = ""
+season = -1
+
+
+""" Returns the file name of any given path """	
+def get_file_name(path):
+		head, tail = ntpath.split(path)
+		return tail or ntpath.basename(head)
 	
-hello
+
+""" Analyzes the filename to find TV show and season"""	
+def analyze_file_name(file):
+	filename = get_file_name(file)
+	return filename
+		
+		
+print analyze_file_name("C/admin/home/Modern-Family.S03E05.XCTV.mkv")
